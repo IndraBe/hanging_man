e79710015cd514881cb634104d6405bf02be56e6
Indra <indraberga@inbox.lv>
2021-10-06 21:27:17 +0300

    Test

---
+++ Hanging_man/Hanging_man/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hanging_man
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
